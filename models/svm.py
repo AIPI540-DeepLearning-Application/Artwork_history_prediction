53fbe2b63a35fd41d777f612f5319d828ef24e1c
Jingwei Li <lijingwei58466809@outlook.com>
2024-02-11 22:18:40 -0500

    update SVM model and the web scraping script

---
+++ models/svm.py
@@ -0,0 +1,64 @@
+import os
+import cv2
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+
+class svm:
+    '''
+    This class is used to train a SVM model on the artwork dataset
+    '''
+    
+    def __init__(self):
+        self.data_folder = "../data_aug/train"
+    
+        
+    def load_data(self, folder):
+        images = []
+        labels = []
+        label_names = sorted(os.listdir(folder))
+        for label_name in label_names:
+            label_folder = os.path.join(folder, label_name)
+            if os.path.isdir(label_folder):
+                for filename in os.listdir(label_folder):
+                    img_path = os.path.join(label_folder, filename)
+                    img = cv2.imread(img_path)
+                    try:                
+                        # convert the image to grayscale
+                        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+                        
+                        # resize the image to 100x100
+                        img = cv2.resize(img, (100, 100))
+                        
+                        # normalize the image
+                        img = img / 255.0 
+                        
+                        # flatten the image to one dimension
+                        images.append(img.flatten())
+                        labels.append(label_name)
+                    except:
+                        print("Error reading file: ", img_path)
+                        
+        return np.array(images), np.array(labels)
+
+    def split_data(self):
+        X, y = self.load_data(self.data_folder)
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+        return X_train, X_test, y_train, y_test
+
+    def train(self):
+        # train the svm model
+        X_train, X_test, y_train, y_test = self.split_data()
+        
+        clf = svm.SVC(kernel='rbf')
+        clf.fit(X_train, y_train)
+
+        # predict on the test set
+        y_pred = clf.predict(X_test)
+
+        # print the classification report
+        accuracy = accuracy_score(y_test, y_pred)
+        print("accuracy:", accuracy)
+        print("\classification report:\n", classification_report(y_test, y_pred))
